5b465d0954bde0eb590a6b24e83a9b130e3edbbb
Mikhael1729 <Mikhael1729@gmail.com>
2020-06-18 13:42:25 -0400

    Install and implement electron

---
+++ src/electron-main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+
+const path = require('path');
+const url = require('url')
+
+let mainWindow;
+function createWindow() {
+  const startUrl =  'http://localhost:3000' || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL(startUrl)
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin')
+    app.quit();
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0)
+    createWindow();
+});
